10719b26941e13b9596da3de2d5887f8cb1a44ad
Aryan <aryanmehtayt2@gmail.com>
2025-01-25 19:20:57 +0530

    Create 4implicttypecasting.cpp

---
+++ 4implicttypecasting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 10;
+	char c = 'a';
+
+	int o = i + c;
+	char d = i + c;
+	cout << o << endl;
+	cout << d << endl;
+
+	int a;
+	char x;
+	a = 1990;
+	x= a;
+	cout << x << endl;  
+
+	x = '0';
+	a = x;
+	cout << a << endl;
+
+	float f = 1.2;
+	a = f;
+	cout << a << endl;
+
+
+
+}
+
