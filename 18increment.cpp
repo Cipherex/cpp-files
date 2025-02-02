0f1933895de3f95ed177e9e24dd3b67e34a04421
Aryan <aryanmehtayt2@gmail.com>
2025-02-03 01:44:19 +0530

    Create 18increment.cpp

---
+++ 18increment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int a = 2 * 3 / 4; //left t0 right precidncy
+	 int b = 10 * 5 / 9;    
+	 cout << a << endl;
+	 cout << b << endl;
+
+	int x = 10;
+	x += 3;
+    
+	x*= 2;
+
+	cout << x-- << endl;
+	cout << --x << endl;
+}
+
