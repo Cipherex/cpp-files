9ee7c498d1278f4f1b8b42518221a48d2f2b4122
Aryan <aryanmehtayt2@gmail.com>
2025-02-12 22:40:44 +0530

    Create 28arrays.cpp

---
+++ 28arrays.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	// Take array input from the user
+
+	int n;
+	cin >> n;
+
+	int input[100]; // should not use constant
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input[i];
+	}
+
+	// Print array
+	for (int i = 0; i < n; i++)
+	{
+		cout << input[i] << endl;
+	} 
+
+	// Largest velement in the array
+
+	int max = INT_MIN;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (input[i] > max)
+		{
+			max = input[i];
+		}
+	}
+
+	cout << "Max : " << max << endl;
+}
