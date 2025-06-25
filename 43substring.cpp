a78bb6c8a0a5665a0008f64186c2c625ca305131
Aryan <aryanmehtayt2@gmail.com>
2025-06-25 10:46:56 +0530

    Create 43substring.cpp

---
+++ 43substring.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+void printSubstrings(char input[]) {
+    for (int i = 0;input[i]!='\0';i++){
+      for (int j = i;input[j]!='\0';j++){
+        for (int k = i; k<=j;k++){
+          cout<<input[k];
+        }
+              cout<<endl;   
+      }
+    }
+}
+
+
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin >> str;
+    printSubstrings(str);
+}
